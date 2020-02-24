18445f7bc7dd41532cfba54de5a69bf21351548d
Anton Bakker <anton.bakker@kadaster.nl>
2020-02-24 16:57:39 +0100

    renamed package

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 from setuptools import setup, find_packages
 
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+req_path = os.path.join(lib_folder, "/requirements.txt")
+install_requires = []
+
+if os.path.isfile(req_path):
+    with open(req_path) as f:
+        install_requires = f.read().splitlines()
+
 with open('README.md') as f:
     README = f.read()
 
 setup(
-    name='capabilities-metadata-converter',
+    name='capabilities-reader',
     version='0.1',
-    description='PDOK Capabilities Metadata converter',
+    description='PDOK (OGC) capabilities documents reader',
     long_description=README,
     author='Anton Bakker',
     author_email='anton.bakker@kadaster.nl',
     packages=find_packages(exclude=('tests', 'docs')),
     setup_requires=['wheel'],
-    install_requires=[
-        'lxml==4.2.1',
-        'iso19119-nl-parser==0.0.2.dev0'
-    ],
+    install_requires=install_requires,
     entry_points='''
         [console_scripts]
-        convert-cap=capabilities_md_converter.cli:main
-        convert-capabilities=capabilities_md_converter.cli:main
+        read-cap=capabilities_reader.cli:main
+        read-capabilities=capabilities_reader.cli:main
     ''',
 )
